46cd1613c6e3883f77791e5eef9ef483c2adc31b
maks <makskilin2004@gmail.com>
2024-05-13 13:33:18 +0500

    refactor module4-task2

--- src/template/empty-list-template.js
+++ src/template/empty-list-template.js
@@ -1,10 +1,12 @@
 function createEmptyListTemplate() {
   return (
-    `<section class="trip-events">
-          <h2 class="visually-hidden">Trip events</h2>
-          <p class="trip-events__msg">Click New Event to create your first point</p>
-      </section>`
+    `<div class="page-body__container">
+  <section class="trip-events">
+    <h2 class="visually-hidden">Trip events</h2>
+    <p class="trip-events__msg">Click New Event to create your first point</p>
+  </section>
+</div>`
   );
 }
 
-export {createEmptyListTemplate};
+export { createEmptyListTemplate };
